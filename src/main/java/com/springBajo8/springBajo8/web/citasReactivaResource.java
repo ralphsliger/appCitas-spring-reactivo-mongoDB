a3d10646c7bb944392d2d784554dbddf0d9f355c
Ralph Sliger <ralphsliger1@gmail.com>
2021-12-30 17:12:44 -0500

    cors

--- src/main/java/com/springBajo8/springBajo8/web/citasReactivaResource.java
+++ src/main/java/com/springBajo8/springBajo8/web/citasReactivaResource.java
@@ -12,6 +12,7 @@ import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @RestController
+@CrossOrigin(origins = "*", methods= {RequestMethod.GET,RequestMethod.POST})
 public class citasReactivaResource {
 
     @Autowired
